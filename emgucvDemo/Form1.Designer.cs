486cd940ca13b3154a68e4a37c13cac1c6e851a7
qiyubrother <13240137763@163.com>
2019-01-03 23:15:15 +0800

    .

--- emgucvDemo/Form1.Designer.cs
+++ emgucvDemo/Form1.Designer.cs
@@ -29,12 +29,35 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(800, 450);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
     }
 }
 
--- emgucvDemo/Form1.cs
+++ emgucvDemo/Form1.cs
@@ -5,16 +5,51 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Threading;
 
 namespace emgucvDemo
 {
     public partial class Form1 : Form
     {
+
+        Emgu.CV.VideoCapture capt;
+        Image frame;
+        VideoWriter videowriter;//
+        int VideoFps;//视频帧率
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            try
+            {
+                if (capt == null)
+                    capt = new Emgu.CV.VideoCapture();
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            if (capt != null)
+            {
+                Application.Idle += new EventHandler(GetFrame);
+            }
+
+            base.OnShown(e);
+        }
+
+        private void GetFrame(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> frame = capt.QueryFrame().ToImage<Bgr, Byte>();
+            imageBox1.Image = frame;
         }
     }
 }
